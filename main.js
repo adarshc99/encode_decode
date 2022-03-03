233a4e9ac49aa162f375ad8ec7add744862e455b
adarsh <adarshsc99@gmail.com>
2022-03-03 15:23:09 +0530

    spaces removed and comments added

--- main.js
+++ main.js
@@ -2,12 +2,10 @@
 var get_key_global;
 var get_text;
 var encrypt_display;
-
-function show_encrpted_text()
+function show_encrpted_text() // function for encryption
 {
     let table_display =  "";
     table_display+=`
-
     <tr>
         <td>${get_text}</td>
         <td>${get_key_global}</td>
@@ -18,15 +16,11 @@ function show_encrpted_text()
     document.getElementById("en_text").value = "";
     document.getElementById("en_key").value = "";
     document.getElementById("encrypted_text").value = "";
-
 }
-
-
-function show_decrypted_text()
+function show_decrypted_text() // function for decryption
 {
     let table_display =  "";
     table_display+=`
-
     <tr>
         <td>${encrypt_display}</td>
         <td>${get_key_global}</td>
@@ -38,15 +32,12 @@ function show_decrypted_text()
     document.getElementById("en_key").value = "";
     document.getElementById("encrypted_text").value = "";
 }
-
-
-function generate_key(get_length)
+function generate_key(get_length) // key genretaion of same length of text
 {
     let key = "";
     for (let index = 0; index < get_length; index++)
     {
-        key+= String.fromCharCode(Math.floor(Math.random()*89 + 33));
-        
+        key+= String.fromCharCode(Math.floor(Math.random()*89 + 33));   
     }
     return key;
 }
@@ -54,7 +45,7 @@ function encypt()
 {
     get_text = document.getElementById("en_text").value;
     document.getElementById("id1").innerHTML = "Encrypt Text";
-    if(!get_text)
+    if(!get_text) // all text fields are necessary
     {
         document.getElementById("display_error").style.display = "block";
     }
@@ -65,15 +56,12 @@ function encypt()
         encrypt_display = "";
         for (let index = 0; index < get_text.length; index++)
         {
-            encrypt_display += String.fromCharCode(get_text.charCodeAt(index)^get_key_global.charCodeAt(index));
-            
+            encrypt_display += String.fromCharCode(get_text.charCodeAt(index)^get_key_global.charCodeAt(index));  
         }
         document.getElementById("encrypted_text").value = encrypt_display;
         document.getElementById("abc").disabled  = false;
         document.getElementById("abc").style.cursor = "context-menu";
-    
         show_encrpted_text();
-
     }
 }
 
@@ -82,12 +70,9 @@ function decrypt()
     document.getElementById("en_text").placeholder = "Enter Text To Decrypt";
     get_key_global = document.getElementById("en_key").value = window. prompt("Enter Key: ");
     document.getElementById("encrypted_text").placeholder = "Your Text is";
-    
-
     if(!get_key_global)
     {
         document.getElementById("display_error").style.display = "block";
-
     }
     else
     {
@@ -102,13 +87,9 @@ function decrypt()
         }
         get_text = document.getElementById("encrypted_text").value = decode;
         show_decrypted_text();
-
     }
-  
 }
-
 function disable(get_id)
 {
-    console.log(get_id);
-    document.getElementById(get_id).style.display = "none";  
+    document.getElementById(get_id).style.display = "none";   // function for X in error message
 }
